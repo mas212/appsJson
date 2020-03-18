064fe4cd79afb405b56f65e09fb4c7e4d996bbb0
mas212 <mas32957@gmail.com>
2020-03-18 11:00:13 +0700

    first commit

---
+++ app/Kategori.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Kategori extends Model
+{
+    protected $table = 'kategoris';
+
+    protected $fillable = [
+    	'name'
+    ];
+}
